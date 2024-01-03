a8809129025ac0e19dc97cb042ef6b6d948d8fc9
emilygknight <emilyknight819@gmail.com>
2024-01-03 04:16:37 -0600

    about me def pops up first when you load the page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,16 @@
 // import { useState } from 'react';
 
+
+const aboutMe = {
+  name: "Emily Knight",
+  description: "Web Developer",
+}
+
 function About() {
   console.log("hello");
- 
+  return <div>
+{aboutMe.name} {aboutMe.description}
+  </div>
 }
 
 export default About;
