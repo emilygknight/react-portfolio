0d8811cf32d7eefc5a13d84579b69d5ddb04daf1
emilygknight <emilyknight819@gmail.com>
2024-01-06 23:49:08 -0600

    added image of resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,20 @@
+// import resumeImage from './assets/images/resume.png';
+
+const myResume = {
+    name: "Emily Knight",
+    description: "Web Developer",
+    image: "../src/assets/images/resume.png"
+  }
+  
+
 function Resume() {
     console.log("hello");
   
     // let [about, setAbout] = useState();
   
-    return <div>
-        Resume
+    return <div className='resume-page'> Resume
+        <img src={myResume.image} alt={myResume.name} />
+
     </div>
   }
   
