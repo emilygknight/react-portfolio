87ff231deafc7b83de1dec1b89d350f74127bfb8
emilygknight <emilyknight819@gmail.com>
2024-01-08 22:41:05 -0600

    aboutme picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,16 @@
 // import { useState } from 'react';
-import mainImage from '../assets/images/0F1A9020.jpg';
+// import mainImage from '../assets/images/0F1A9020.jpg'
 
 const aboutMe = {
   name: "Emily Knight",
   description: "Web Developer",
-  image: mainImage
+  image: './0F1A9020.jpg'
 }
 
 function About() {
   console.log("hello");
   return <div className="aboutme-page">
-    <img src={mainImage} alt={aboutMe.name} />
+    <img src={aboutMe.image} alt={aboutMe.name} />
     <h2>{aboutMe.name}</h2>
     <p>{aboutMe.description}</p>
   </div>
